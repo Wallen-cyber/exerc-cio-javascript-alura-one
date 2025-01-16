57cedc39b1107266df2e25e33e649b0149bec82e
Wallen-cyber <wallen.oliveira@yahoo.com.br>
2025-01-16 07:32:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+alert('Boas vindas ao nosso site!');
+let nome= "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos'; alert('mensagemDeErro');
+prompt('Digite seu nome');
+prompt('digite sua idade');
+if(idade >= 25){
+    alert('Pode tirar habilitação!');
+}
